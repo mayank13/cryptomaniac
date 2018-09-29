b18095729a799885ffd86cd6492d816e1d92cc31
Mayank Jain <mayank13@gmail.com>
2018-09-29 10:08:55 +0530

    Create boiler plate for API's

---
+++ api/controllers/ExchangeController.js
@@ -0,0 +1,11 @@
+/**
+ * ExchangeController
+ *
+ * @description :: Server-side logic for managing Exchanges
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
